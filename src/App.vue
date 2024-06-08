a30ee9726dea42988bbe3ad1f3d92943f9ff3a0c
Danila Glazov <43675993+ExileeY@users.noreply.github.com>
2024-06-08 11:35:25 +0400

    Added basic UI (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePage />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import HomePage from './components/game/HomePage.vue'
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body, html, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+
+.flex-column {
+  display: flex;
+  flex-direction: column;
+}
+
+.flex-row {
+  display: flex;
+  flex-direction: row;
+}
+
+.gap-10 { gap: 10px; }
+
+.flex-center {
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ src/components/game/HomePage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home-page flex-column flex-center">
+    <div class="title">
+      ROCK – PAPER – SCISSORS
+    </div>
+
+    <div class="choices flex-row gap-10">
+      <div @click="play('rock')">
+        1
+      </div>
+
+      <div @click="play('paper')">
+        2
+      </div>
+
+      <div @click="play('scissors')">
+        3
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const play = (choice) => {
+  console.log('userChoise:', choice)
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+.home-page {
+  width: 100%;
+  height: 100%;
+  font-weight: normal;
+  font-style: normal;
+}
+
+.home-page .title {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 45px;
+  text-align: left;
+  line-height: 60px;
+  color: #36006C;
+}
+</style>
